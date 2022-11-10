418ea216d0a973aaab7cff836bc3d07146c77cd9
sumin0725 <suminxx12x@gmail.com>
2022-11-11 03:05:10 +0900

    ADD : 게시물 좋아요 기능 구현

--- src/pages/yunsumin/Main/Components/Feed.js
+++ src/pages/yunsumin/Main/Components/Feed.js
@@ -9,6 +9,8 @@ const Feed = props => {
   // 댓글로 입력한 값을 배열에 저장해줄 빈배열
   const [commentList] = useState([]);
 
+  const [feedLick, setFeedLick] = useState(false);
+
   const saveCommentValue = event => {
     setCommentValue(event.target.value);
   };
@@ -18,6 +20,15 @@ const Feed = props => {
   const post = () => {
     commentList.push(commentValue);
     setCommentValue('');
+  };
+
+  const clickLick = () => {
+    if (feedLick == false) {
+      setFeedLick(true);
+    } else {
+      setFeedLick(false);
+      console.log(feedLick);
+    }
   };
 
   return (
@@ -38,15 +49,15 @@ const Feed = props => {
       </div>
       <div className="commentBox">
         <div className="lickBar">
-          <div>
-            <button type="button" className="hartButton">
-              <svg
-                className="hart"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z" />
-              </svg>
+          <div className="commentLickBar">
+            <button type="button" className="hartButton" onClick={clickLick}>
+              <i
+                className={
+                  feedLick
+                    ? 'fa-solid fa-heart fa-2x'
+                    : 'fa-regular fa-heart fa-2x'
+                }
+              />
             </button>
             <svg
               className="comment"
@@ -66,7 +77,9 @@ const Feed = props => {
             <img alt="강아지1 이미지" src="/images/yunsumin/dog1.jpg" />
           </div>
           <p>console_0님 외</p>
-          <b className="lieckr">{feed.userLick}명</b>
+          <b className="lieckr">
+            {feedLick ? feed.userLick + 1 : feed.userLick}명
+          </b>
           <p>이 좋아합니다</p>
         </div>
         <div className="title">
