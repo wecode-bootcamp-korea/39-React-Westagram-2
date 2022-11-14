cd3fde297025054e2bba7205f4d9176da6469051
a young <xx.36.5MHz@gmail.com>
2022-11-14 10:04:11 +0900

    Refactor : login 페이지 수정

--- src/pages/songayoung/Login/AyLogin.js
+++ src/pages/songayoung/Login/AyLogin.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 export default function AyLogin() {
-  const check = () => {
+  const loginInfo = () => {
     fetch('http://10.58.52.237:3000/auth/signin', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json;charset=utf-8' },
@@ -16,15 +16,15 @@ export default function AyLogin() {
       .then(data => console.log(data));
   };
 
-  const [email, setId] = useState('');
-  const [password, setPw] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  const saveUserId = event => {
-    setId(event.target.value);
+  const saveUserEmail = event => {
+    setEmail(event.target.value);
   };
-  const saveUserPw = event => {
-    setPw(event.target.value);
+  const saveUserPassword = event => {
+    setPassword(event.target.value);
   };
 
   const isValid = email.indexOf('@') !== -1 && password.length >= 8;
@@ -35,12 +35,10 @@ export default function AyLogin() {
       : alert('비밀번호가 일치하지 않습니다.');
   };
 
-  const disable = isValid ? false : true;
-
   return (
     <>
       <title>Westagram</title>
-      <div className="wrap">
+      <div className="login">
         <div className="container">
           <div className="inner">
             <form className="form">
@@ -50,7 +48,7 @@ export default function AyLogin() {
                   id="id"
                   type="text"
                   placeholder="전화번호, 사용자 이름 또는 이메일"
-                  onChange={saveUserId}
+                  onChange={saveUserEmail}
                 />
               </div>
               <div className="inputBox">
@@ -58,24 +56,24 @@ export default function AyLogin() {
                   id="password"
                   type="password"
                   placeholder="비밀번호"
-                  onChange={saveUserPw}
+                  onChange={saveUserPassword}
                 />
               </div>
               <div className="buttonBox">
                 <button
                   type="button"
-                  disabled={disable}
+                  disabled={!isValid}
                   className="btn"
-                  onClick={check}
+                  onClick={loginInfo}
                 >
                   로그인
                 </button>
               </div>
             </form>
             <div className="passwordForget">
-              <a>
+              <Link>
                 <span>비밀번호를 잊으셨나요?</span>
-              </a>
+              </Link>
             </div>
           </div>
         </div>
